dee43e818bc7fc3f13d8be4c731c71b5561b20f0
Shubham Gautam <sg2199203@gmail.com>
2020-10-29 08:40:52 +0530

    Services page updated

--- js/main.js
+++ js/main.js
@@ -20,3 +20,22 @@ function navSmall() {
         });
     }
 }
+
+/*Owl Carousel*/
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    center: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+})
