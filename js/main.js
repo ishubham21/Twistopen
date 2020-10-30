0e0347a6892441299eb8782af4060d63342b5c93
Shubham Gautam <sg2199203@gmail.com>
2020-10-30 10:51:45 +0530

    Popover js enabeled

--- js/main.js
+++ js/main.js
@@ -1,24 +1,9 @@
-var carItem = document.querySelectorAll(".item");
-var popup = document.querySelector(".popup");
-var popCross = document.getElementById("cross-close");
-var popText = document.getElementById("pop-text");
-for (let i = 0; i < carItem.length; i++) {
-    carItem[i].addEventListener("click", () =>{
-        popup.classList.add("popup-display-true");
-    });
-    if (i === 1) {
-        popText.textContent = "Fantastic! We can help you with your product. Share your email." ;
-    }
-    else if (i === 2){
-        popText.textContent = "Hello";
-    }
-}
-popCross.addEventListener("click", () => {
-    popup.classList.remove("popup-display-true");
-});
+
+
+navSmall();
+carouselPop();
 
 /*This function is to toggle the navbar for smaller screens*/
-navSmall();
 function navSmall() {
     var navCont = document.querySelector(".nav-container");
     var navSm = document.querySelector(".nav-sm");
@@ -37,4 +22,45 @@ function navSmall() {
             navCont.classList.remove("cross-icon");
         });
     }
+}
+/*This function ensures displaying of different texts upon clicking of different elements*/
+function carouselPop() {
+    var carItem = document.querySelectorAll(".item");
+    var popup = document.querySelector(".popup");
+    var popCross = document.getElementById("cross-close");
+    var popText = document.getElementById("pop-text");
+    for (let i = 0; i < carItem.length; i++) {
+        carItem[i].addEventListener("click", () => {
+            popup.classList.add("popup-display-true");
+        });
+    }
+    carItem[0].addEventListener("click", () => {
+        popText.innerHTML = "Awesome! <br> We can help you build it right the first time. Share your email."
+    });
+    carItem[1].addEventListener("click", () => {
+        popText.innerHTML = "Fantastic! <br> We can help you with your product. Share your email.";
+    });
+    carItem[2].addEventListener("click", () => {
+        popText.innerHTML = "Right on! <br> We can help you uncover valuable user insights.Share your email.";
+    });
+    carItem[3].addEventListener("click", () => {
+        popText.innerHTML = "Lovely! <br> We can help you jazz your product up.Share your email.";
+    });
+    carItem[4].addEventListener("click", () => {
+        popText.innerHTML = "Great! <br> We can help you manage your resources better.Share your email.";
+    });
+
+    // carItem[1].addEventListener("click", () => {
+    //     popup.classList.add("popup-display-true");
+    //     popText.innerHTML = "Fantastic! <br> We can help you with your product. Share your email.";
+          
+    // });
+    // carItem[2].addEventListener("click", () => {
+    //     popup.classList.add("popup-display-true");
+    //     popText.innerHTML = "Hello! <br> We can help you with your product. Share your email.";
+    // });
+    popCross.addEventListener("click", () => {
+        popup.classList.remove("popup-display-true");
+    });
+    
 }
