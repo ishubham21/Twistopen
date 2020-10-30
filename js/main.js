fdcd96d37ed7c06214a81c85b3ab37307b4fc2c2
Shubham Gautam <sg2199203@gmail.com>
2020-10-30 09:49:52 +0530

    Popover added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+var carItem = document.querySelectorAll(".item");
+var popup = document.querySelector(".popup");
+var popCross = document.getElementById("cross-close");
+var popText = document.getElementById("pop-text");
+for (let i = 0; i < carItem.length; i++) {
+    carItem[i].addEventListener("click", () =>{
+        popup.classList.add("popup-display-true");
+    });
+    if (i === 1) {
+        popText.textContent = "Fantastic! We can help you with your product. Share your email." ;
+    }
+    else if (i === 2){
+        popText.textContent = "Hello";
+    }
+}
+popCross.addEventListener("click", () => {
+    popup.classList.remove("popup-display-true");
+});
 
 /*This function is to toggle the navbar for smaller screens*/
 navSmall();
@@ -20,4 +38,3 @@ function navSmall() {
         });
     }
 }
-
